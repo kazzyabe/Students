8834fda4ff725b572f48b2e0f63af0ac1fb7dee4
kazuki <kazuki@KY-MacBook-Air.local>
2016-05-28 15:03:02 -0700

    Framework finished

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
 class StudentsController < ApplicationController
   def index
+    @student = Student.all
   end
 
   def show
@@ -9,6 +10,10 @@ class StudentsController < ApplicationController
   def new
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
   def create
     @student = Student.new(student_params)
 
@@ -16,17 +21,25 @@ class StudentsController < ApplicationController
     redirect_to @student
   end
 
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update(student_params)
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    
+    redirect_to students_path
+  end
+
   private
     def student_params
       params.require(:student).permit(:name, :weight, :height, :color, :gpa)
     end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
